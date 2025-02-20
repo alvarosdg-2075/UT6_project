a0792ec2266980a35336926e0fbb04e9cd6fdbf7
alvarosdg-2075 <asanzdeech@educacion.navarra.es>
2025-02-20 20:03:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ function toggleOptions() {
     var options = document.getElementById('optionsPanel');
     options.style.display = options.style.display === 'none' ? 'block' : 'none';
 }
+
 function changeText() {
     document.getElementById('paragraph').innerText = "You personalized this text!";
 }
+
 function changeBgColor() {
     document.body.style.backgroundColor = 'lightblue';
 }
+
 function changeTextStyle() {
-    document.getElementById('paragraph').style.fontSize = '20px';
-    document.getElementById('paragraph').style.fontWeight = 'bold';
+    var paragraph = document.getElementById('paragraph');
+    paragraph.style.fontSize = '20px';
+    paragraph.style.fontWeight = 'bold';
 }
